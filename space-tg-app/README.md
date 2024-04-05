1185f9893c88c59e643c53c6f61e543fda03ebf8
EldarDanilovich <ildar.nasrov@gmail.com>
2024-04-06 00:15:40 +0500

    add script to index.html

--- space-tg-app/README.md
+++ space-tg-app/README.md
@@ -9,3 +9,10 @@ need installed mkcert
 `brew install mkcert` for installing
 
 `mkdir -p .cert && mkcert -key-file ./.cert/localhost-key.pem -cert-file ./.cert/localhost.pem 'tg-mini-app.local'`
+
+#### Deploy
+
+`yarn build`
+
+copy all to hosting
+`scp -rp ./dist/* ildar-nasrov@ildar-nasrov.myjino.ru:/home/users/i/ildar-nasrov/domains/tgapp.dobromam.ru`
--- space-tg-app/src/main.tsx
+++ space-tg-app/src/main.tsx
@@ -7,14 +7,28 @@ import { appTheme } from "./theme/theme.ts";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n";
 import WebApp from "@twa-dev/sdk";
+import { SDKProvider, type SDKInitOptions } from "@tma.js/sdk-react";
 
 WebApp.ready();
+
+export function Root() {
+  const options: SDKInitOptions = {
+    acceptCustomStyles: true,
+    cssVars: true,
+  };
+
+  return (
+    <SDKProvider options={options}>
+      <App />
+    </SDKProvider>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={appTheme}>
       <BrowserRouter>
-        <App />
+        <Root />
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
