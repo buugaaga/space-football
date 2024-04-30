0d5be4c0961143a48f23b87b144f9508cf09d86b
AliKhan <baybutov95@mail.ru>
2024-05-01 00:56:14 +0500

    add background, make header, add fake elements to check scroll

--- main-landing/src/App.tsx
+++ main-landing/src/App.tsx
@@ -1,10 +1,35 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box, Container, Heading } from "@chakra-ui/react";
+
+const bgImgURL = "/images/corner.jpg";
 
 function App() {
   return (
-    <Flex>
-      <p>Hello, world!</p>
-    </Flex>
+    <Box
+      bgImage={bgImgURL}
+      minHeight="100vh"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      bgAttachment="fixed"
+    >
+      <Box as="header" bgColor="#ffffff">
+        <Container maxW="1200px" margin="0 auto">
+          <Flex h="110px" alignItems="center">
+            <Heading size="lg">Space Football</Heading>
+          </Flex>
+        </Container>
+      </Box>
+      <Box
+        h="1200px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Heading size="lg" color="#ffffff">
+          Space Football
+        </Heading>
+      </Box>
+      <Box h="800px" bgColor="#ffffff"></Box>
+    </Box>
   );
 }
 
