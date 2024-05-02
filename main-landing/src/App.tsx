347b02346c0e47e012f7b0dbf6a80e16f0bb9acf
AliKhan <baybutov95@mail.ru>
2024-05-03 03:27:19 +0500

    replace background, make cover

--- main-landing/src/App.tsx
+++ main-landing/src/App.tsx
@@ -1,6 +1,6 @@
 import { Flex, Box, Container, Heading } from "@chakra-ui/react";
 
-const bgImgURL = "/images/corner.jpg";
+const bgImgURL = "/images/footballer.jpg";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       minHeight="100vh"
       bgRepeat="no-repeat"
       bgSize="cover"
+      bgPosition="center"
       bgAttachment="fixed"
     >
       <Box as="header" bgColor="#ffffff">
@@ -18,15 +19,27 @@ function App() {
           </Flex>
         </Container>
       </Box>
-      <Box
-        h="1200px"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Heading size="lg" color="#ffffff">
-          Space Football
-        </Heading>
+      <Box as="main">
+        <Container maxW="1200px" margin="0 auto">
+          <Flex
+            h="calc(100vh - 110px)"
+            justifyContent="center"
+            alignItems="center"
+            direction="column"
+          >
+            <Box
+              fontSize="72px"
+              marginBottom="54px"
+              fontWeight="bold"
+              color="#ffffff"
+            >
+              Professionals rely on numbers!
+            </Box>
+            <Box color="#ffffff" fontSize="46px">
+              All in one statistical solution
+            </Box>
+          </Flex>
+        </Container>
       </Box>
       <Box h="800px" bgColor="#ffffff"></Box>
     </Box>
