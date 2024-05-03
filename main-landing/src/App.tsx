d91a5a2e5ac9772634ffcf5de62a0e7a5d2e60af
AliKhan <baybutov95@mail.ru>
2024-05-04 02:04:28 +0500

    make video section

--- main-landing/src/App.tsx
+++ main-landing/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       bgPosition="center"
       bgAttachment="fixed"
     >
-      <Box as="header" bgColor="#ffffff">
+      <Box as="header" bgColor="white">
         <Container maxW="1200px" margin="0 auto">
           <Flex h="110px" alignItems="center">
             <Heading size="lg">Space Football</Heading>
@@ -31,17 +31,32 @@ function App() {
               fontSize="72px"
               marginBottom="54px"
               fontWeight="bold"
-              color="#ffffff"
+              color="white"
             >
               Professionals rely on numbers!
             </Box>
-            <Box color="#ffffff" fontSize="46px">
+            <Box color="white" fontSize="46px">
               All in one statistical solution
             </Box>
           </Flex>
         </Container>
+        <Box bgColor="white">
+          <Container maxW="1200px" margin="0 auto" bgColor="white">
+            <Flex h="720px" pt="90px" pb="90px" justify="center" align="center">
+              <iframe
+                width="960"
+                height="540"
+                src="https://www.youtube.com/embed/g_ERlr_Y_H8?si=hKA0RF3XLSharIKX"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </Flex>
+          </Container>
+        </Box>
       </Box>
-      <Box h="800px" bgColor="#ffffff"></Box>
     </Box>
   );
 }
