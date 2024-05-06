9bd99230fd1ffc6f0a38143fce7aa563f6e21a33
AliKhan <baybutov95@mail.ru>
2024-05-07 00:51:21 +0500

    make footer

--- main-landing/src/App.tsx
+++ main-landing/src/App.tsx
@@ -1,4 +1,13 @@
-import { Flex, Box, Container, Heading } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Container,
+  Heading,
+  Text,
+  Input,
+  Textarea,
+  Button,
+} from "@chakra-ui/react";
 
 const bgImgURL = "/images/footballer.jpg";
 
@@ -56,54 +65,54 @@ function App() {
             </Flex>
           </Container>
           <Container maxW="760px" h="1280px" py="150px">
-            <Box fontSize="72px" fontWeight="bold" textAlign="center" mb="72px">
+            <Text fontSize="72px" as="b" textAlign="center" mb="72px">
               Who we are?
-            </Box>
-            <Box fontSize="20px" mb="54px">
+            </Text>
+            <Text fontSize="20px" mb="54px">
               SportsBase is a new digital project dedicated to set up a football
               analytics and video analysis system. The company provides a wide
               range of services covering all possible customer requests,
               including a video platform, statistical post-match reports, live
               breakdown with the minimal delay, raw data, player fitness
               tracking reports, and analytical player reports.
-            </Box>
-            <Box fontSize="26px" fontWeight="bold" mb="36px">
+            </Text>
+            <Text fontSize="26px" as="b" mb="36px">
               OUR MISSION
-            </Box>
-            <Box fontSize="20px" mb="54px">
+            </Text>
+            <Text fontSize="20px" mb="54px">
               We aim at developing football with the contemporary IT solutions
               using machine learning, neural networks, customized and
               user-friendly software which will facilitate the work for coaches,
               analysts as well as journalists and commentators. The SportsBase
               services are fully customizable, so all the users will manage to
               find what they need.
-            </Box>
-            <Box fontSize="26px" fontWeight="bold" mb="36px">
+            </Text>
+            <Text fontSize="26px" as="b" mb="36px">
               OUR QUALITY
-            </Box>
-            <Box fontSize="20px" mb="54px">
+            </Text>
+            <Text fontSize="20px" mb="54px">
               More than 200 experienced ex-InStat employess, own independent
               production department, own methodology of full Live analysis.
-            </Box>
-            <Box fontSize="26px" fontWeight="bold" mb="36px">
+            </Text>
+            <Text fontSize="26px" as="b" mb="36px">
               RELIABILITY
-            </Box>
-            <Box fontSize="20px" mb="54px">
+            </Text>
+            <Text fontSize="20px" mb="54px">
               SportsBase is a well-structured company with over 200 people in
               staff all around the world.
-            </Box>
+            </Text>
           </Container>
         </Box>
         <Box bgColor="#f5f5f5">
-          <Container maxW="1200px" h="3300px" pt="135px" pb="165px">
+          <Container maxW="1200px" maxH="3300px" pt="135px" pb="165px">
             <Flex direction="column" gap="50px">
               <Flex justify="space-between" align="center">
                 <Box as="img" src="/images/1.png" maxW="48%"></Box>
                 <Box>
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     SPORTSBASE SOLUTIONS
-                  </Box>
-                  <Box mb="36px" fontSize="18px">
+                  </Text>
+                  <Text mb="36px" fontSize="18px">
                     <p>• SportsBase football video platform;</p>
                     <p>• Technical match filming;</p>
                     <p>• Video and advanced statistics;</p>
@@ -111,23 +120,23 @@ function App() {
                     <p>• Statistical live data collection;</p>
                     <p>• Player fitness tracking reports;</p>
                     <p>• Raw data delivered in live mode.</p>
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     Services to be launched in the short term:
                     <p>• 24/7 technical support;</p>
                     <p>• Live fitness tracking;</p>
                     <p>• Analytical opponent’s prescout reports;</p>
                     <p>• Post-match referee reports;</p>
                     <p>• Offline and online tagging software.</p>
-                  </Box>
+                  </Text>
                 </Box>
               </Flex>
               <Flex justify="space-between" align="center">
                 <Box>
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     SPORTSBASE PLATFORM FOR COACHES
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     <p>• Video breakdown of own and opponent’s matches;</p>
                     <p>
                       • Various options to review a match: full match video,
@@ -145,17 +154,17 @@ function App() {
                       • Quick video download with the 1080p or 720p resolution;
                     </p>
                     <p>• Customizable statistical spreadsheets.</p>
-                  </Box>
+                  </Text>
                 </Box>
                 <Box as="img" src="/images/2.png" maxW="48%"></Box>
               </Flex>
               <Flex justify="space-between" align="center">
                 <Box as="img" src="/images/3.png" maxW="48%"></Box>
                 <Box pl="100px">
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     SPORTSBASE PLATFORM FOR PLAYER RECRUITMENT
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     <p>
                       • Filter players by their statistics to find the best
                       candidates;
@@ -167,31 +176,31 @@ function App() {
                     <p>
                       • Share any video in two clicks with your team via email.
                     </p>
-                  </Box>
+                  </Text>
                 </Box>
               </Flex>
               <Flex justify="space-between" align="center">
                 <Box>
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     STATISTICAL REPORTS
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     <p>• Real-time statistical match analysis;</p>
                     <p>• Statistical post-match team and player report;</p>
                     <p>• Team and tournament summary reports;</p>
                     <p>• New advanced metrics developed;</p>
                     <p>• Data quality control.</p>
-                  </Box>
+                  </Text>
                 </Box>
                 <Box as="img" src="/images/4.png" maxW="48%"></Box>
               </Flex>
               <Flex justify="space-between" align="center">
                 <Box as="img" src="/images/5-1.png" maxW="48%"></Box>
                 <Box>
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     PLAYER FITNESS TRACKING REPORTS
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     <p>
                       • Fitness tracking data about your own team as well as the
                       opponents;
@@ -206,25 +215,83 @@ function App() {
                     </p>
                     <p>• New metrics combining event and fitness data;</p>
                     <p>• Proven data collection methodology.</p>
-                  </Box>
+                  </Text>
                 </Box>
               </Flex>
               <Flex justify="space-between" align="center">
                 <Box pr="50px">
-                  <Box fontSize="26px" fontWeight="bold" mb="24px">
+                  <Text fontSize="26px" as="b" mb="24px">
                     RAW DATA
-                  </Box>
-                  <Box fontSize="18px">
+                  </Text>
+                  <Text fontSize="18px">
                     <p>• Over 2500 metrics;</p>
                     <p>• Data available in XML and JSON formats;</p>
                     <p>
                       • Tailor-made assistance and guidance on how to use the
                       data for a website or on social media.
                     </p>
-                  </Box>
+                  </Text>
                 </Box>
                 <Box as="img" src="/images/5-2.png" maxW="48%"></Box>
               </Flex>
+            </Flex>
+          </Container>
+        </Box>
+        <Box bgColor="white">
+          <Container maxW="1200px" maxH="650px" pt="150px">
+            <Flex justify="start" pb="150px">
+              <Box
+                objectFit="cover"
+                transform="scale(1.25)"
+                alignSelf="center"
+                opacity="0.4"
+                mr="8%"
+                as="img"
+                src="/images/letter_s.png"
+              ></Box>
+              <Box flexBasis="460px">
+                <Text fontSize="30px" as="b">
+                  Contact us
+                </Text>
+                <Text fontSize="18px" mb="30px">
+                  office@sportsbase.world
+                </Text>
+                <Text fontSize="16px" mb="5px">
+                  Your email
+                </Text>
+                <Input
+                  maxW="460px"
+                  mb="20px"
+                  variant="filled"
+                  placeholder="mail@example.com"
+                />
+                <Text fontSize="16px" mb="5px">
+                  Your name
+                </Text>
+                <Input
+                  maxW="460px"
+                  mb="20px"
+                  variant="filled"
+                  placeholder="John Smith"
+                />
+                <Text fontSize="16px">Your message</Text>
+                <Textarea
+                  maxW="460px"
+                  variant="filled"
+                  placeholder="Write your appeal"
+                  mb="20px"
+                />
+                <Button
+                  color="white"
+                  width="160px"
+                  height="50px"
+                  background="linear-gradient(0.12031726607834739turn, rgba(0, 0, 0, 1) 1%, rgba(0, 95, 174, 1) 72%);"
+                  transition="0.3s ease all"
+                  borderRadius="16px"
+                >
+                  Button
+                </Button>
+              </Box>
             </Flex>
           </Container>
         </Box>
